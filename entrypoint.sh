1a208304d9a76d7de5f1b0b22ebfc26847498e25
Danila Vershinin <ciapnz@gmail.com>
2024-09-06 19:21:32 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ branch=""
 extra_args=""
 github_token=""
 working_directory=""
+cache_dir="${XDG_CACHE_HOME:-${GITHUB_WORKSPACE}/.cache}"
 
 # Parse the named arguments
 while [ "$#" -gt 0 ]; do
@@ -48,14 +49,24 @@ while [ "$#" -gt 0 ]; do
   esac
 done
 
-cd $working_directory
+# Ensure the working directory exists; create it if it doesn't
+if [ ! -d "$working_directory" ]; then
+  echo "Working directory does not exist: $working_directory. Creating it."
+  mkdir -p "$working_directory"
+fi
 
-# if $repository is empty, use the current repository
+cd "$working_directory" || exit
+
+# Set the cache directory to a writable path
+export XDG_CACHE_HOME="$cache_dir"
+mkdir -p "$XDG_CACHE_HOME"
+
+# If $repository is empty, use the current repository
 if [ -z "$repository" ]; then
   repository="$GITHUB_REPOSITORY"
 fi
 
-# if $github_token is not empty, override the built-in token
+# If $github_token is not empty, override the built-in token
 if [ -n "$github_token" ]; then
   export GITHUB_TOKEN="$github_token"
 fi
